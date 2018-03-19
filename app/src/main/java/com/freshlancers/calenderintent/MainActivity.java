871082c2a32ac9e3364706d0f1a837899e73f13d
Ashith V L <ashithvl@gmail.com>
2018-03-19 18:21:50 +0530

    Initial commit

---
+++ app/src/main/java/com/freshlancers/calenderintent/MainActivity.java
@@ -0,0 +1,56 @@
+package com.freshlancers.calenderintent;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import permissions.dispatcher.NeedsPermission;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    // Add an event to the calendar of the user.
+    @OnClick(R.id.create)
+    public void addEvent(View view) {
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2018, 3, 21, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2018, 3, 22, 8, 30);
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(CalendarContract.Events.TITLE, "Yoga")
+                .putExtra(CalendarContract.Events.DESCRIPTION, "Group class")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "The gym")
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+                .putExtra(Intent.EXTRA_EMAIL, "ashithvl@gmail.com,testing@example.com");
+        startActivity(intent);
+
+    }
+
+}
